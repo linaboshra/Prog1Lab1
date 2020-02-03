0e9ac054a7bcc784efe88311dbfddade1c51c783
linaboshra <linabotros@yahoo.com>
2020-02-03 08:44:17 -0500

    MyAgeIs.java

---
+++ MyAgeIs.java
@@ -0,0 +1,15 @@
+public class MyAgeIs {
+
+    public static void main(String[] args) {
+
+        int currentYear = 2020;
+        int yearOfBirth = 2001;
+         
+        int age;
+
+        age = currentYear - yearOfBirth;
+
+        System.out.println("My age is " + age);
+    }
+
+}
